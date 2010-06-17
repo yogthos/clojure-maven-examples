6731936146d71e1d8fb653a42a2186d072e2d5d2
Ghast <yogthos@gmail.com>
2010-06-16 21:08:32 -0400

    cleaned up check-bounds

--- mandelbrot/src/org/yogthos/mandelbrot.clj
+++ mandelbrot/src/org/yogthos/mandelbrot.clj
@@ -3,56 +3,56 @@
            (javax.swing JPanel JFrame) 
            (java.awt.image BufferedImage BufferStrategy))) 
 
-;(set! *warn-on-reflection* true) 
+(set! *warn-on-reflection* true) 
 
-(let [width          (float 640)
-       height        (float 640)
-       max-steps     (float 32)
-       color-scale   (float (quot 255 max-steps))
-       height-factor (/ 2.5 height)
-       width-factor  (/ 2.5 width)]
+(let [width         (float 640)
+      height        (float 640)
+      max-steps     (float 64)
+      color-scale   (float (quot 255 max-steps))
+      height-factor (/ 2.5 height)
+      width-factor  (/ 2.5 width)
+      bailout       (float 4.0)]
 
    (defn on-thread [#^Runnable f] (.start (new Thread f)))
 
-   (defn check-bounds [x y]
-     (let [f2 (float 2.0)
-           f4 (float 4.0)]
-       
-       (loop [px    (float x)
-              py    (float y)
-              zx    (float 0.0)
-              zy    (float 0.0)
-              zx2   (float 0.0)
-              zy2   (float 0.0)
-              value (float 0.0)]
-         
-         (if (and (< value max-steps) (< (+ zx2 zy2) f4))
-           (let [new-zy  (float  (+ (* (* f2 zx) zy) py))
-                 new-zx  (float  (+ (- zx2 zy2) px))
-                 new-zx2 (float (* new-zx new-zx))
-                 new-zy2 (float (* new-zy new-zy))]
-             (recur px py new-zx new-zy new-zx2 new-zy2 (inc value)))
-           (if (== value max-steps) 0 value)))))
+   (defn check-bounds [cr ci]
+       (loop [i  (int 1)              
+              zi (float 0.0)
+              zr (float 0.0)]
 
+        (let [zr2  (float (* zr zr))
+              zi2  (float (* zi zi))
+              temp (float (* zr zi))]
+         (cond
+           (> (float (+ zi2 zr2)) bailout) i
+           
+           (> i max-steps) 0
+ 
+           :default (recur (inc i)
+                           (float (+ (+ temp temp) ci))
+                           (float (+ (- zr2 zi2) cr)) 
+         )))))
+   
   (defn draw-line [#^Graphics g y]
     (let [dy (float (- 1.25 (* y height-factor)))]
       (doseq [x (range 0 width)]
         (let [dx (float (- (* x width-factor) (float 2.0)))]
+          
           (let [value   (check-bounds dx dy)
                 scaled  (Math/round (float (* value color-scale)))
                 xscaled (Math/round (float (* x (/ 255 width))))]
+            
             (if (> value  0)
-              (.setColor g
-                  (new Color 255 (- 255 scaled) scaled))
-              (.setColor g 
-                  (new Color xscaled (- 255 xscaled) xscaled)))
+              (.setColor g (new Color 255 (- 255 scaled) scaled))              
+              (.setColor g (new Color xscaled (- 255 xscaled) xscaled)))
+            
             (.drawRect g  x y 0 0)))))) 
 
    (defn draw-lines
      ([buffer g] (draw-lines buffer g height))
      ([#^BufferStrategy buffer g y]
-        (doseq [y (range 0 y)]                    
-          (on-thread (draw-line g y))
+        (doseq [y (range y)]              
+          (on-thread (draw-line g y))                    
           (.show buffer))))
 
    (defn draw [#^Canvas canvas]
