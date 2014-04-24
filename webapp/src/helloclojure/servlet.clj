754d217d156f67f837418a3efe3c09b74c24b900
Yogthos <dmitri.sotnikov@gmail.com>
2014-04-23 23:57:44 -0400

    updated servlet example

--- webapp/src/helloclojure/servlet.clj
+++ webapp/src/helloclojure/servlet.clj
@@ -1,24 +1,16 @@
-(ns helloclojure.servlet           
-	(:require (org.danlarkin [json :as json]))                                                             
-  (:gen-class :extends javax.servlet.http.HttpServlet))             
-                                                                                                                              
-(defn -doGet                                                  
-  [_ request response]     
-  (.setContentType response "text/html")                                                                                                   
-  (let [json-req (json/decode-from-str (.getParameter request "Name"))
-  			w        (.getWriter response)]                                                                 
-    (.println w 
-			(str "<html>"
-			"<head>" 
-			 "<title>Hello World!</title>"
-			"</head>" 
-			 "<body>" 
-			"<h1>Hello "
-			(:name json-req) 
-			;(.getParameter request "Name")
-			"</h1>" 
-			"</body>" 
-			"</html>")))) 
-			
-(defn -doPost [_ request response]
-	(-doGet nil request response))			
+(ns helloclojure.servlet
+	(:require [hiccup.page :refer [html5]])
+  (:gen-class :extends javax.servlet.http.HttpServlet))
+
+(defn -doGet
+  [_ request response]
+  (.setContentType response "text/html")
+  (.. response
+      getWriter
+      (println
+        (html5
+          [:head [:title "Hello World"]]
+          [:body [:h1 "Hello " (.getParameter request "Name")]]))))
+
+(defn -doPost [this request response]
+	(-doGet this request response))
